cc9108a0c5661e4e08b85e68ae48fbf76857ab77
Abdul Kanjo <Abdul.kanjo@gmail.com>
2018-08-16 14:25:33 -0500

    first vue project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input placeholder="Bot Name"/>
+    <input placeholder="Attack Value"/>
+    <input placeholder="Health Value"/>
+    <button>Submit</button>
+    <button>Clear</button>
+   </div>
+
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "hello"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
